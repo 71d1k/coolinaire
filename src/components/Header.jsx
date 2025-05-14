b7efada030d0e04336c224b09626467f36c1919b
71d1k <sydykovbekzhan@gmail.com>
2025-05-14 20:43:17 +0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// Header.js
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
+import "../styles/Header.css";
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header>
-      <h1>Coolinaire</h1>
+    <header className={`header ${isScrolled ? "scrolled" : ""}`}>
+      <Link to="/" className="logo">
+        <img src={logo} alt="Coolinaire Logo" />
+        <span>Coolinaire</span>
+      </Link>
       <nav>
-        <Link to="/">Recipes</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
+        <Link to="/" className="nav-link">Главная</Link>
+        <Link to="/contact" className="nav-link">Отклик</Link>
       </nav>
     </header>
   );
