c08fd94a0c021d6a9b3e62c00069346e8616ce09
71d1k <sydykovbekzhan@gmail.com>
2025-05-14 20:43:34 +0300

    recipelist

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import RecipeCard from './RecipeCard';
+import '../styles/RecipeList.css';
 
 const RecipeList = () => {
+  const [searchTerm, setSearchTerm] = useState('');
   const recipes = [
-  // рецепты
-    { title: 'Паста Карбонара ', image: 'src/assets/pasta-carbonara.jpg', link: '/pasta-carbonara' },
-    { title: 'Тайский том-ям', image: 'src/assets/thai-tom-yam.jpg', link: '/thai-tom-yam' },
-    { title: 'Греческий салат', image: 'src/assets/greek-salad.jpg', link: '/greek-salad' },
-    { title: 'Чили кон карне', image: 'src/assets/chili-con-carne.jpg', link: '/chili-con-carne' },
-    { title: 'Рататуй', image: 'src/assets/ratatouille.jpg', link: '/ratatouille' },
-    { title: 'Бешбармак', image: 'src/assets/beshbarmak.jpg', link: '/beshbarmak' },
-    { title: 'Тирамису', image: 'src/assets/tiramisu.jpg', link: '/tiramisu' },
-    { title: 'Окрошка', image: 'src/assets/okroshka.jpg', link: '/okroshka' },
+    { title: 'Паста Карбонара', image: 'src/assets/pasta-carbonara.jpg', link: 'pasta-carbonara' },
+    { title: 'Тайский том-ям', image: 'src/assets/thai-tom-yam.jpg', link: 'thai-tom-yam' },
+    { title: 'Греческий салат', image: 'src/assets/greek-salad.jpg', link: 'greek-salad' },
+    { title: 'Чили кон карне', image: 'src/assets/chili-con-carne.jpg', link: 'chili-con-carne' },
+    { title: 'Рататуй', image: 'src/assets/ratatouille.jpg', link: 'ratatouille' },
+    { title: 'Бешбармак', image: 'src/assets/beshbarmak.jpg', link: 'beshbarmak' },
+    { title: 'Тирамису', image: 'src/assets/tiramisu.jpg', link: 'tiramisu' },
+    { title: 'Окрошка', image: 'src/assets/okroshka.jpg', link: 'okroshka' },
   ];
 
+  const filteredRecipes = recipes.filter(recipe =>
+    recipe.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div>
-      <h2>Recipe Index</h2>
+    <div className="recipe-list-container">
+      <h1 className="recipe-list-title">Рецепты</h1>
+      <input
+        type="text"
+        placeholder="Поиск рецептов"
+        value={searchTerm}
+        onChange={e => setSearchTerm(e.target.value)}
+        className="recipe-search-input"
+      />
       <div className="recipe-list">
-        {recipes.map((recipe, index) => (
-          <RecipeCard key={index} recipe={recipe} />
+        {filteredRecipes.map(recipe => (
+          <Link to={`/recipe/${recipe.link}`} key={recipe.link} className="recipe-card-link">
+            <RecipeCard title={recipe.title} image={recipe.image} />
+          </Link>
         ))}
       </div>
     </div>
